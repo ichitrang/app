ed0a0a53458d4df1ef9c72f72eee4d6ccebfc2bc
chitrangji <chitrang.j@cronberry.com>
2025-07-21 11:27:52 +0530

    first frontend commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple"
+    )
+}
