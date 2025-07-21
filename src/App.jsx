569de9f599967a4431a6eef99783efbdc38562d5
ichitrang <ichitrangjain@gmail.com>
2025-07-21 15:11:30 +0530

    edit past folder

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Login from "./Login"; // ✅ This line pulls in Login component
-
-function App() {
-  return <Login />;
-}
-
-export default App;
--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-    return (
-        <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple"
-    )
-}
