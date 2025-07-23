c8f9c51feeb61fad27ec1c234a02c95dffd7b2dd
ichitrang <ichitrangjain@gmail.com>
2025-07-23 11:47:21 +0530

    idex.js_corrected

--- front_end/src/index.jsx
+++ front_end/src/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 const Login = () => {
     return (
-        <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple"
-    )
-}
+        <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <h1 className="text-white text-3xl font-bold">Login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
