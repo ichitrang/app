7e702f865aab5e52fbb95d0cbad0b8d12929ce3e
ichitrang <ichitrangjain@gmail.com>
2025-07-23 14:23:18 +0530

    Updated registration logic and UI; added Dashboard and SignUp pages

--- back_end/src/main/java/com/example/demo/repository/UserRepository.java
+++ back_end/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,5 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.demo.model.User;
+import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<User, String> {}
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
